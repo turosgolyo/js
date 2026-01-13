bda108b175e4fea548313dfdfe656fb41e088967
turosgolyo <papp.dani2206@gmail.com>
2026-01-13 09:32:36 +0100

    labrador

--- event-manager/public/scripts/events.js
+++ event-manager/public/scripts/events.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-let events = [];
-const cardsContainer = document.getElementById('cards');
-
-try {
-    fetch('/api/events', {
-        method: 'GET',
-        headers: {
-            'Content-type': 'application/json',
-        },
-    })
-        .then((response) =>
-            response
-                .json()
-                .then((data) => (events = data))
-                .then(() => loadEvents()),
-        )
-        .catch((error) => {
-            console.error(error);
-            alert('Load failed: ' + (error.message || 'Network error'));
-        });
-} catch (error) {
-    console.error('Error fetching events data:', error);
-}
-
-function loadEvents() {
-    cardsContainer.innerHTML = events
-        .map((event) => {
-            cardsContainer.innerHTML = `<div class="col-lg-4 col-md-6 col-sm-12 mb-2">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">${event.name}</h5>
-                <p class="card-text">
-                    ${event.description}
-                </p>
-                <p class="card-text">
-                    ${event.date}
-                </p>
-                <p class="card-text">
-                    ${event.location}
-                </p>
-                <a href="#" class="btn btn-outline-secondary">Register</a>
-            </div>
-        </div>
-    </div>`;
-        })
-        .join('');
-}
-=======
 const cards = document.getElementById('cards');
 
 const loadEvents = () => {
@@ -75,7 +26,6 @@ const loadEvents = () => {
                             </div>
                         </div>
                     `;
-
                     cards.appendChild(card);
                 });
 
@@ -112,4 +62,3 @@ function registerToEvent(eventId, userId) {
 }
 
 document.addEventListener('DOMContentLoaded', loadEvents);
->>>>>>> 4b603ea6ba4d51e1905026d41763c3e6b1e53c0e
